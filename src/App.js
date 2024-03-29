d2810a9fe9fba7e984200a49978293ab8d4aff13
divyesh_pc <divyeshvora08@gmail.com>
2024-03-29 16:49:53 +0530

    first message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import io from "socket.io-client";
 
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:3001");
 function App() {
+  const [username, setUsername] = useState("");
+  const [roomId, setRoomId] = useState("");
+
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    socket.on("recived_message", (recivedObj) => {
+      setMessageList((prev) => [...prev, recivedObj]);
+    });
+  }, [socket]);
+
+  const roomHandler = () => {
+    socket.emit("join_room", roomId);
+    if (!(username === "" && roomId === "")) {
+    }
+  };
+
+  const messageHandler = async () => {
+    const dataObj = {
+      roomId,
+      author: username,
+      currentMessage,
+      time: new Date(Date.now()),
+    };
+    await socket.emit("send_message", dataObj);
+  };
+
+  console.log("messageList", messageList);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="user..."
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="room..."
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
+        />
+        <button onClick={roomHandler}>join</button>
+      </div>
+      <br />
+      <div>
+        <input
+          type="text"
+          placeholder="enter your message..."
+          value={currentMessage}
+          onChange={(e) => setCurrentMessage(e.target.value)}
+        />
+        <button onClick={messageHandler}>send</button>
+      </div>
+    </>
   );
 }
-
 export default App;
